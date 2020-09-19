58d1a501e7b447bbd14af80556b8d3b5cea7f760
daeunSong <daeun7250@gmail.com>
2020-09-19 13:09:00 +0900

    Clean code

--- sl1m/rbprm/surfaces_from_planning.py
+++ sl1m/rbprm/surfaces_from_planning.py
@@ -1,26 +1,12 @@
 from numpy import arange
 from narrow_convex_hull import getSurfaceExtremumPoints, removeDuplicates, normal
-from pinocchio import XYZQUATToSe3
+from pinocchio import XYZQUATToSE3
 from numpy import array
-from sl1m.problem_definition import LF
+from sl1m.problem_definition import LF, RF
 
-MAX_SURFACE = 0.1 # about forth the size of the foot area
+MAX_SURFACE = 0.1 # about forth the size of the foot area 
+ROBOT_NAME = 'talos'
 
-def rotationMatrixFromConfigs(configs):
-    R = []
-    for config in configs:
-        q_rot = config[3:7]
-        R.append(XYZQUATToSe3([0,0,0]+q_rot).rotation)
-    return R     
-  
-def listToArray (seqs):
-    nseq = []; nseqs= []
-    for seq in seqs:
-        nseq = []
-        for surface in seq:
-            nseq.append(array(surface).T)
-        nseqs.append(nseq)
-    return nseqs
 
 def area(s):
     a = 0
@@ -28,18 +14,38 @@ def area(s):
       a += (s[i][0]*s[i+1][1])-(s[i][1]*s[i+1][0])
     return abs(a * 0.5)
 
+def listToArray (seqs):
+    nseq = []; nseqs= []
+    for seq in seqs:
+        if seq == []:
+            nseqs.append(nseq)
+        else:
+            nseq = []
+            for surface in seq:
+                if surface != []:
+                    nseq.append(array(surface).T)
+            nseqs.append(nseq)
+    return nseqs
+
+def getRotationsFromConfigs(configs):
+    R = []
+    for config in configs:
+        q_rot = config[3:7]
+        R.append(XYZQUATToSE3([0,0,0]+q_rot).rotation)
+    return R    
+
 def getContactsNames(rbprmBuilder,i,q):
-    if i % 2 == LF : # left leg moving
-        step_contacts = rbprmBuilder.clientRbprm.rbprm.getCollidingObstacleAtConfig(q,'talos_lleg_rom') 
-    else :
-        step_contacts = rbprmBuilder.clientRbprm.rbprm.getCollidingObstacleAtConfig(q,'talos_rleg_rom')
+    if i % 2 == LF : # left leg 
+        step_contacts = rbprmBuilder.clientRbprm.rbprm.getCollidingObstacleAtConfig(q, ROBOT_NAME + '_lleg_rom') 
+    elif i % 2 == RF : # right leg
+        step_contacts = rbprmBuilder.clientRbprm.rbprm.getCollidingObstacleAtConfig(q,ROBOT_NAME + '_rleg_rom')
     return step_contacts
 
 def getContactsIntersections(rbprmBuilder,i,q):
       if i % 2 == LF : # left leg
-          intersections = rbprmBuilder.getContactSurfacesAtConfig(q,'talos_lleg_rom')  
-      else : # right leg
-          intersections = rbprmBuilder.getContactSurfacesAtConfig(q,'talos_rleg_rom')
+          intersections = rbprmBuilder.getContactSurfacesAtConfig(q,ROBOT_NAME + '_lleg_rom')  
+      elif i % 2 == RF : # right leg
+          intersections = rbprmBuilder.getContactSurfacesAtConfig(q,ROBOT_NAME + '_rleg_rom')
       return intersections
 
 # get all the contact surfaces (pts and normal)
@@ -54,9 +60,13 @@ def getAllSurfacesDict (afftool) :
     surfaces_dict = dict(zip(all_names, all_surfaces)) # map surface names to surface points
     return surfaces_dict
 
-def getSurfacesFromGuideContinuous(rbprmBuilder,ps,afftool,pId,viewer = None,step = 1.,useIntersection= False):
-    pathLength = ps.pathLength(pId) #length of the path
-    discretizationStep = 0.5 # step at which we check the colliding surfaces
+def getSurfacesFromGuideContinuous(rbprmBuilder,ps,afftool,viewer = None,step = 1.,useIntersection= False):
+    if viewer : 
+        from tools.display_tools import displaySurfaceFromPoints  # tool from hpp-rbprm
+    
+    window_size = 0.5 # smaller step at which we check the colliding surfaces
+    pathId = ps.numberPaths() -1
+    pathLength = ps.pathLength(pathId) #length of the path
     
     # get surface information
     surfaces_dict = getAllSurfacesDict(afftool) # map surface names to surface points
@@ -65,92 +75,100 @@ def getSurfacesFromGuideContinuous(rbprmBuilder,ps,afftool,pId,viewer = None,ste
     current_phase_end = step
     end = False
     i = 0
+
     while not end: # for all the path
-        #print "Looking for surfaces for phase "+str(len(seqs))+" for t in ["+str(t)+" ; "+str(current_phase_end)+" ] "
+        phase_contacts = []
         phase_contacts_names = []
         while t < current_phase_end: # get the names of all the surfaces that the rom collide while moving from current_phase_end-step to current_phase_end
-            q = ps.configAtParam(pId, t)
-            step_contacts = getContactsNames(rbprmBuilder,i,q)
-            for contact_name in step_contacts : 
-                if not contact_name in phase_contacts_names:
-                    phase_contacts_names.append(contact_name)
-            t += discretizationStep
+            q = ps.configAtParam(pathId, t)
+            step_contacts = getContactsIntersections(rbprmBuilder,i,q)
+            step_contacts_names = getContactsNames(rbprmBuilder,i,q)
+            # do not consider the duplicates yet
+            phase_contacts += step_contacts
+            phase_contacts_names += step_contacts_names
+            t += window_size
+            assert len(phase_contacts) == len(phase_contacts_names)
         # end current phase
-        # get all the surfaces from the names and add it to seqs: 
+        
+        seq = []
         if useIntersection : 
-            intersections = getContactsIntersections(rbprmBuilder,i,q)
-        phase_surfaces = []
-        for name in phase_contacts_names:
-            surface = surfaces_dict[name][0]
-            if useIntersection and area(surface) > MAX_SURFACE : 
-                if name in step_contacts : 
-                    intersection = intersections[step_contacts.index(name)]
-                    phase_surfaces.append(intersection)
-            else :
-                phase_surfaces.append(surface) # [0] because the last vector contain the normal of the surface
-        #print "There was "+str(len(phase_contacts_names))+" surfaces in contact during this phase."
-        phase_surfaces = sorted(phase_surfaces) # why is this step required ? without out the lp can fail
-        phase_surfaces_array = [] # convert from list to array, we cannot do this before because sorted() require list
-        for surface in phase_surfaces:
-            phase_surfaces_array.append(array(surface).T)
-        #print "phase_surfaces_array = ",phase_surfaces_array    
-        seqs.append(phase_surfaces_array)
+            for i, contact in enumerate(phase_contacts):
+                if area(contact) > MAX_SURFACE:
+                    seq.append(contact)
+                else:
+                    seq.append(surfaces_dict[phase_contacts_names[i]][0])
 
-        # increase values for next phase
+        # get all the surfaces from the names and add it to seqs: 
+        else :
+            for name in phase_contacts_names:
+                surface = surfaces_dict[name][0]
+                if surface not in seq: # check if there is duplicate
+                    seq.append(surface)
+        
+        sorted(seq)
+        seqs.append(seq)
+
+        # increase value for the next phase
         t = current_phase_end
-        i += 1 
-        if current_phase_end == pathLength:
-            end = True
         current_phase_end += step
-        if current_phase_end >= pathLength:
-            current_phase_end = pathLength
-    # end for all the guide path
+        i += 0 # phase number
 
-    #get rotation matrix of the root at each discretization step
+        if t == pathLength:
+            current_phase_end = pathLength+0.01
+        if t > pathLength:
+            end = True
+    # end of all guide path
+
+    #get rotation matrix of the root at each step
+    seqs = listToArray(seqs)
     configs = []
     for t in arange (0, pathLength, step) :
-        configs.append(ps.configAtParam(pId, t)) 
-    R = rotationMatrixFromConfigs(configs)
+        configs.append(ps.configAtParam(pathId, t)) 
+    R = getRotationsFromConfigs(configs)
+
     return R,seqs
 
 
-def getSurfacesFromGuide(rbprmBuilder,ps,afftool,pId,viewer = None,step = 0.6,useIntersection = False):
+def getSurfacesFromGuide(rbprmBuilder,ps,afftool,viewer = None,step = 1.,useIntersection = False):
     if viewer : 
         from tools.display_tools import displaySurfaceFromPoints  # tool from hpp-rbprm
-    pathLength = ps.pathLength(pId) #length of the path
+
+    pathId = ps.numberPaths() -1
+    pathLength = ps.pathLength(pathId) #length of the path
     configs = []
     # get configuration along the path
     for s in arange (0, pathLength, step) :
-        configs.append(ps.configAtParam(pId, s)) 
+        configs.append(ps.configAtParam(pathId, s)) 
 
     # get surface information
-    surface_dict = getAllSurfacesDict(afftool)
+    surfaces_dict = getAllSurfacesDict(afftool)
     
     # get surface candidate at each discretization step
     # suppose that we start with the left leg
-    seqs = []; 
-    for i, config in enumerate(configs):    
+    seqs = []
+    for i, q in enumerate(configs):    
         seq = [] 
-        contacts = getContactsIntersections (rbprmBuilder,i,config)
-        contact_names = getContactsNames (rbprmBuilder,i,config)
+        contacts = getContactsIntersections (rbprmBuilder,i,q)
+        contact_names = getContactsNames (rbprmBuilder,i,q)
+        assert len(contacts) == len(contact_names)
 
-        for i,contact in enumerate(contacts):
+        for j, contact in enumerate(contacts):
+            if viewer:
+                displaySurfaceFromPoints(viewer,contact,[0,0,1,1])
             if contact != []:
-                print area(contact)
                 if useIntersection and area(contact) > MAX_SURFACE:
-                    seq.append(contact) # changed it to list to make it easier to remove duplicates
-                    #seq.append(array(contact).T)
+                    seq.append(contact) 
+                    # if viewer:
+                    #     displaySurfaceFromPoints(viewer,contact,[0,0,1,1])
                 else:
-                    seq.append(surface_dict[contact_names[j]][0])
-                if viewer:
-                    displaySurfaceFromPoints(viewer,contact,[0,0,1,1])
+                    seq.append(surfaces_dict[contact_names[j]][0])
+
         seqs.append(seq)
-        #indices.append(index_list)
     
     # remove duplicates          
     for i, seq in enumerate(seqs): seqs[i] = removeDuplicates(seq)
     
     seqs = listToArray(seqs) # change the format from list to array
-    R = rotationMatrixFromConfigs(configs)
+    R = getRotationsFromConfigs(configs)
     return R,seqs
-
+    
