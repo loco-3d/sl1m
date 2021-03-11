f8871336d871c4f7366104f830cd61cd55d87440
daeunSong <daeun7250@gmail.com>
2021-03-12 00:13:29 +0900

    Update stand alone scenarios demos

--- sl1m/stand_alone_scenarios/complex.py
+++ sl1m/stand_alone_scenarios/complex.py
@@ -3,13 +3,12 @@ import numpy as np
 
 from sl1m.constants_and_tools import *
 
-from numpy import array, asmatrix, matrix, zeros, ones
-from numpy import array, dot, stack, vstack, hstack, asmatrix, identity, cross, concatenate
+from numpy import array, asmatrix, concatenate, cross, dot, hstack, identity, matrix, ones, stack, vstack, zeros
 from numpy.linalg import norm
 
 from sl1m.planner import *
 
-from constraints import *
+from sl1m.stand_alone_scenarios.constraints import *
 
 
 start  = [[-3., 0.4  , 0.  ], [-2.7 ,  0.4, 0. ], [-2.7 , 0.1, 0.  ], [-03., 0.1, 0.  ], ]
@@ -38,17 +37,17 @@ rub9  = [[ -0.35, -0.15 , 0.05 ], [-0.86 , -0.15, 0.05], [-0.86, 0.52, 0.05 ], [
 
 all_surfaces = [start, floor, step1, step2, step3, step4,step5,step6, step7, bridge, platfo, rub8, rub9,rub7, rub75, rub6, rub5, rub4, rub3, rub2]
 
-arub9  = array(rub9).T 
-arub8  = array(rub8).T 
-arub75  = array(rub75).T 
-arub7  = array(rub7).T 
-arub6  = array(rub6).T 
-arub5  = array(rub5).T 
-arub4  = array(rub4).T 
-arub3  = array(rub3).T 
-arub2  = array(rub2).T 
-astart  = array(start).T 
-afloor  = array(floor).T 
+arub9  = array(rub9).T
+arub8  = array(rub8).T
+arub75  = array(rub75).T
+arub7  = array(rub7).T
+arub6  = array(rub6).T
+arub5  = array(rub5).T
+arub4  = array(rub4).T
+arub3  = array(rub3).T
+arub2  = array(rub2).T
+astart  = array(start).T
+afloor  = array(floor).T
 astep1  = array(step1).T
 astep2  = array(step2).T
 astep3  = array(step3).T
@@ -77,17 +76,17 @@ surfaces = [[arub2],[arub3,arub2],[arub4],[arub5],[arub6],[arub7],[arub75],allru
 def gen_stair_pb():
     kinematicConstraints = genKinematicConstraints(left_foot_constraints, right_foot_constraints)
     relativeConstraints = genFootRelativeConstraints(right_foot_in_lf_frame_constraints, left_foot_in_rf_frame_constraints)
-            
+
     nphases = len(surfaces)
-    
-    p0 = [array([-2.7805096486250154, 0.33499999999999996, 0.]), array([-2.7805096486250154, 0.145,0.])];    
+
+    p0 = [array([-2.7805096486250154, 0.33499999999999996, 0.]), array([-2.7805096486250154, 0.145,0.])];
     res = { "p0" : None, "c0" : None, "nphases": nphases}
-    
+
     phaseData = [ {"moving" : i%2, "fixed" : (i+1) % 2 , "K" : [copyKin(kinematicConstraints) for _ in range(len(surfaces[i]))], "relativeK" : [relativeConstraints[(i)%2] for _ in range(len(surfaces[i]))], "S" : surfaces[i] } for i in range(nphases)]
     res ["phaseData"] = phaseData
-    return res 
-    
-    
+    return res
+
+
 import mpl_toolkits.mplot3d as a3
 import matplotlib.colors as colors
 import scipy as sp
@@ -98,24 +97,22 @@ def draw_rectangle(l, ax):
     cy = [c[1] for c in lr]
     cz = [c[2] for c in lr]
     ax.plot(cx, cy, cz)
-    
+
 def draw_scene(surfaces, ax = None, color = "p"):
-    if ax is None:        
+    if ax is None:
         fig = plt.figure()
         ax = fig.add_subplot(111, projection="3d")
     [draw_rectangle(l,ax) for l in all_surfaces]
     return ax
-    
-    
-    
-############# main ###################    
+
+
+
+############# main ###################
 
 if __name__ == '__main__':
     from sl1m.fix_sparsity import solveL1, solveMIP
-    
+
     pb = gen_stair_pb()
-    solveMIP(pb, surfaces, MIP = True, draw_scene = draw_scene, plot = True)
+    solveMIP(pb, surfaces, draw_scene, plot = True)
     pb = gen_stair_pb()
     solveL1(pb, surfaces, draw_scene, plot = True)
-    
-    
