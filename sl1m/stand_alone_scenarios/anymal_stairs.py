a0b2a14dafb3986659cdf5512ed5edef850e659f
Wolfgang Merkt <w.merkt@gmail.com>
2021-12-13 15:58:56 +0000

    anymal_stairs: Add option for SL1M, add costs, remove use of INSTALL_HPP_DIR
    - Adds costs to optimisation
    - Adds option to alternatively use SL1M, but deactivate it (since it
    does not converge)
    - Removes use/need of INSTALL_HPP_DIR environment variable

--- sl1m/stand_alone_scenarios/anymal_stairs.py
+++ sl1m/stand_alone_scenarios/anymal_stairs.py
@@ -4,7 +4,7 @@ from time import perf_counter as clock
 from pathlib import Path
 import anymal_rbprm
 
-from sl1m.generic_solver import solve_MIP
+from sl1m.generic_solver import solve_L1_combinatorial, solve_MIP
 from sl1m.problem_definition import Problem
 from sl1m.stand_alone_scenarios.surfaces.stair_surfaces import quadruped_surfaces_gait as surfaces
 from sl1m.stand_alone_scenarios.surfaces.stair_surfaces import scene
@@ -13,12 +13,20 @@ import sl1m.tools.plot_tools as plot
 
 if __name__ == '__main__':
     # Parameters
+    USE_SL1M = False   # SL1M does not converge, MIP does.
     USE_COM = True
     GAIT = [np.array([1, 1, 1, 0]), np.array([1, 1, 0, 1]), np.array([1, 0, 1, 1]), np.array([0, 1, 1, 1])]
-
     anymal_rbprm_path = Path(anymal_rbprm.__file__).resolve().parent.parent.parent.parent.parent / "share" / "anymal-rbprm"
     paths = [str(anymal_rbprm_path / "com_inequalities" / "feet_quasi_flat" / "anymal_"),
              str(anymal_rbprm_path / "relative_effector_positions" / "anymal_")]
+ 
+ 
+    COSTS = {
+        "posture": [1.0],
+        "step_size": [1.0, [0.3, 0.0]],
+        "final_com": [10.0, [1.2, 0.3, 0.9]]
+    }
+ 
     limbs = ['RHleg', 'RFleg', 'LHleg', 'LFleg']
     others = ['RH_ADAPTER_TO_FOOT', 'RF_ADAPTER_TO_FOOT', 'LH_ADAPTER_TO_FOOT', 'LF_ADAPTER_TO_FOOT']
     suffix = "_effector_frame_quasi_static_upscale.obj"
@@ -40,7 +48,10 @@ if __name__ == '__main__':
     pb.generate_problem(R, surfaces, GAIT, initial_contacts, q_init)
     t_3 = clock()
 
-    result = solve_MIP(pb, com=USE_COM)
+    if USE_SL1M:
+        result = solve_L1_combinatorial(pb, com=USE_COM, costs=COSTS)
+    else:
+        result = solve_MIP(pb, com=USE_COM, costs=COSTS)
     t_end = clock()
 
     print(result)
